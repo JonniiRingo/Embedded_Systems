e8cedaa14f7b63e8c83cb133ace78338ea495d34
Jonnii Ringo <jonathansamuelsEl@gmail.com>
2026-06-21 11:02:35 -0700

    Bitshift section started

---
+++ src/50_bitshift.cpp
@@ -0,0 +1,40 @@
+ #include <iostream>
+#include <stdio.h>
+
+
+// int var1 = 10, var2 = 3, var3 = 5;
+
+
+//Bitshift Operators 8-bit is from 0 -255
+uint8_t var1 = 0b00000000;
+uint8_t var2 = 0b00000000;
+uint8_t var3 = 0b00000000;
+
+int main (){
+
+
+
+
+    // printf("\nbefore var1 = %d", var1);
+
+    // // Increment
+    // // var1 = var2++ + ++var3; 
+
+    // // Division 
+    // // var1 = var1 / var2; 
+
+    // //Modulus 
+    // var1 = var1 % var2;
+
+
+    printf("\nvar1 = %d", var1);
+    printf("\nvar2 = %d", var2);
+    printf("\nvar3 = %d", var3);
+
+
+
+
+
+    
+return 0;
+}
