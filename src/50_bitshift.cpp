c9db1941b04d71516208dd9f81482104a6226f74
Jonnii Ringo <jonathansamuelsEl@gmail.com>
2026-06-21 11:21:52 -0700

    Bitshift complete

--- src/50_bitshift.cpp
+++ src/50_bitshift.cpp
@@ -6,9 +6,10 @@
 
 
 //Bitshift Operators 8-bit is from 0 -255
-uint8_t var1 = 0b00000000;
-uint8_t var2 = 0b00000000;
-uint8_t var3 = 0b00000000;
+uint8_t var1    = 0b00000111;
+uint8_t var2    = 0b00001000;
+uint8_t var3    = 0b00010001;
+uint8_t result  = 0b00000000;
 
 int main (){
 
@@ -26,13 +27,34 @@ int main (){
     // //Modulus 
     // var1 = var1 % var2;
 
+    // Or
+    // result = var1 | var2;
+
+    //XOR
+    // result = var1 ^ var3;
+
+    //NOT 
+    // result = ~var1;
+
+    //Bitshift : 3
+    // result = var1 >> 1;
+
+    //Bitshift : 56 
+    // result = var1 << 3;
+
+    //Bitshift Overflow: 0
+    // result = var1 << 8;
+
+    // Bitshift shorthand 
+    result = var1 << 2;
+    var1 <<=  2;
 
     printf("\nvar1 = %d", var1);
     printf("\nvar2 = %d", var2);
     printf("\nvar3 = %d", var3);
 
 
-
+    printf("\nresult  = %d", result);
 
 
     
