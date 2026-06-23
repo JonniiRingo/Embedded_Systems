0875caab5609aa1a3590c2169b2093835ec39ad4
Jonnii Ringo <jonathansamuelsEl@gmail.com>
2026-06-23 08:55:21 -0700

    Compound conditional statements using logical and comparison operators inline with each other

--- src/51_conditionals.cpp
+++ src/51_conditionals.cpp
@@ -4,24 +4,28 @@
 
 
 
-int a = 1, b = 2, c = 3; 
+int a = 1, b = 1, c = 2; 
 
 int main(){
 
-    if (a == b){
-        printf("\na = b");
-    }
+    // if (a == b){
+    //     printf("\na = b");
+    // }
 
-    if (a > b){
-        printf("\na > b");
-    }
+    // if (a > b){
+    //     printf("\na > b");
+    // }
 
-    if (b < c){
-        printf("\nb < c");
-    }
+    // if (b < c){
+    //     printf("\nb < c");
+    // }
 
-    if (a != b){
-        printf("\na != b -> %d", (a!=b));
+    // if (a != b){
+    //     printf("\na != b -> %d", (a!=b));
+    // }
+
+    if ((a == b) && (b != c)){
+        printf("\n(a == b) && (b != c) is true!  -> %d",  ((a == b) && (b != c))    ); 
     }
 
 
---
+++ src/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+    // if (a > b){
+    //     printf("\na > b");
+    // }
+
+    // if (b < c){
+    //     printf("\nb < c");
+    // }
+
+    // if (a != b){
+    //     printf("\na != b -> %d", (a!=b));
+    // }
