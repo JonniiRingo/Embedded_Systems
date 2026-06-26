6ddc25c139886969a0cf76b2c432405b6ea8f4be
Jonnii Ringo <jonathansamuelsEl@gmail.com>
2026-06-25 23:49:13 -0700

    Looping though a char array with a for loop

--- src/52_loops.cpp
+++ src/52_loops.cpp
@@ -11,19 +11,51 @@ int main(){
 
 int i = 0;
 
+
+// While loop 
     while( ++i <= 100 ){
         cout << "\ni =  " << i << endl;
-        // i++;
+        if ( i == 5){
+            break;
+        }
     }
 
-    cout << "\nAfter while\n" << endl;
+    cout << "\nAfter while\n\n" << endl;
 
 int j = 10;
 
-    do {
-        cout << "j = " << j << endl;
-    }while ( --j >= 0);
 
+// Do while
+    do {
+        cout << "\nj = " << j << endl;
+    }while ( --j >= 0);
+cout << "\nAfter do while\n\n" << endl;
+
+
+    int d;
+// For loop 
+cout << "\nBefore for loop" << endl;
+    for (int d = 0; d < 10; d++ ){
+
+    if (d == 4){
+        continue;
+    }
+
+    cout << "\nd = " << d << endl;
+}
+
+
+ cout << "\nAfter the for loop. " << endl;
+
+ char myName[] = {'J', 'o' , 'n', 'a', 't', 'h', 'a', 'n', 0};
+
+ for (int l = 0; l < 8; l++){
+    cout << myName[l];
+ }
+
+ cout << endl;
 
     return 0;
 }
+
+
