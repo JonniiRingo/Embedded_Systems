9610ada2c1ddcbf6394e28e526865fc456cabde0
Jonnii Ringo <jonathansamuelsEl@gmail.com>
2026-06-27 11:41:07 -0700

    Switch statement

---
+++ src/53_switchStatements.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+
+
+
+
+int main(){
+    for(int index = 0; index < 10; index ++){
+        switch(index){
+            case 1:
+            {
+                cout << "In case 1. Index = " << index << endl;
+            } break; 
+
+            case 2:
+            {
+                cout << "In case 2. Index = " << index << endl;
+            } break;
+
+            case 3: 
+            {
+                cout << "In case 3. Index = " << index << endl;
+            } break;
+        }
+    }
+
+
+
+
+    return 0;
+}
