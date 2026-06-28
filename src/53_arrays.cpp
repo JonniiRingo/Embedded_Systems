b8a3ee70f5291cddf082b27ca213e02696561c65
Jonnii Ringo <jonathansamuelsEl@gmail.com>
2026-06-28 13:51:02 -0700

    Started the arrays file

---
+++ src/53_arrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+#include <stdio.h>
+
+
+int16_t playerScore [ 16 ];
+
+int main(){
+
+    for (int index = 0; index < 16; index++){
+        
+    }
+
+
+
+    return 0;
+}
