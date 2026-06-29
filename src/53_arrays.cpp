e34aeb90d478bfb610391b5a9e3ae4986058e284
Jonnii Ringo <jonathansamuelsEl@gmail.com>
2026-06-29 10:18:36 -0700

    Added arrays file

--- src/53_arrays.cpp
+++ src/53_arrays.cpp
@@ -3,12 +3,28 @@ using namespace std;
 #include <stdio.h>
 
 
-int16_t playerScore [ 16 ];
+
+
+int16_t playerScores [ 16 ]= {1,1,2,3,5,8,13,21,0,0,0,0,0,0,0,0};
 
 int main(){
 
-    for (int index = 0; index < 16; index++){
-        
+    int index = 0;
+
+    // while (index++ < 15){
+
+    // for (int index = 0; index < 16; index++){
+
+
+    // for each loop
+    for (auto element : playerScores){
+    // cout << "PlayerScores [ n  ] = " << playerScores[index] << endl;
+
+    // For each syntax
+    cout << "PlayerScores [ n  ] = " << element << endl;
+
+    // c way to print it
+    // printf("\nplayerScores[ %d ] = %d", index, playerScores[index]); 
     }
 
 
