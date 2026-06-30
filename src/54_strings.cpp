ba519590413502a90c0786864f2e71dcb331c260
Jonnii Ringo <jonathansamuelsEl@gmail.com>
2026-06-30 12:47:01 -0700

    Functions intro

--- src/54_strings.cpp
+++ src/54_strings.cpp
@@ -9,7 +9,14 @@ char myName[] = "Jonnii Ringo";
 char yourName[] ="Marty McFly";
 
 
+// Functions
 
+
+
+int addInts(int x, int y){
+    int sum = x + y;
+    return sum;
+}
 
 
 int main(){
@@ -20,9 +27,14 @@ cout << "my name is " << myName << " and the length of this string is " << lengt
 
 strcpy(myName, yourName);
 
+memset (myName, 'x', 3);
+
 cout << "my name is " << myName << " and the length of this string is " << length << endl;
 
+int a = 1;
+int b = 7;
 
+cout << addInts(a,b) << endl;
 
     return 0;
 }
