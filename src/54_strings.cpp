8ff5182c998c93fcce46f8cc66100227da59f6e2
Jonnii Ringo <jonathansamuelsEl@gmail.com>
2026-06-30 11:59:15 -0700

    Copy function demonstrated

--- src/54_strings.cpp
+++ src/54_strings.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 #include <stdio.h>
+#include <cstring>
 
 
 
 char myName[] = "Jonnii Ringo";
+char yourName[] ="Marty McFly";
 
 
 
@@ -15,6 +17,11 @@ int main(){
 int length = strlen(myName) + 1;
 
 cout << "my name is " << myName << " and the length of this string is " << length << endl;
+
+strcpy(myName, yourName);
+
+cout << "my name is " << myName << " and the length of this string is " << length << endl;
+
 
 
     return 0;
